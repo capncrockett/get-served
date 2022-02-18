5f417bfac6df422f09db8c9ce967cda728116504
capncrockett <capncrockett@gmail.com>
2022-02-18 12:54:38 -0800

    Change uvicorn ports to 5000 (instead of default 8000). Be able to start server via pipenv.

---
+++ server.py
@@ -0,0 +1,34 @@
+from imp import reload
+import uvicorn
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+
+async def hello(request):
+    return JSONResponse({'message': '😎'})
+
+
+async def posted(request):   # is this a request if it's a POST?
+    announcement = 'posted'
+    return JSONResponse({'message': f'Yo shit got {announcement}!'})
+    # return JSONResponse({'message': 'Posted'})
+
+
+# Allow all incoming request origins.
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=['*'])
+]
+
+# Starlette works as a router and then demands a cors authentication.
+app = Starlette(debug=True, routes=[
+    Route('/hello', hello),
+    Route('/posted', posted)
+], middleware=middleware)
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="127.0.0.1",
+                port=5000, log_level="info", reload=True)
+# app()
